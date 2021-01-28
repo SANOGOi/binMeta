e0e7ce6076ff5a9c0638f1c7b47a6c44a47ce95a
SANOGOi <issasanogo007@gmail.com>
2021-01-28 01:36:57 +0100

    Add tests hammingDistanceTo, randomSelectInNeighbour, diff

--- Data.java
+++ Data.java
@@ -1089,6 +1089,7 @@ public class Data implements Comparable<Data>
       System.out.println(D02.withCurrentBitFlipped());
       while (D02.hasNextBit())  System.out.println(D02.withNextBitFlipped());
       System.out.println();
+      System.out.println("D02 " + D02);
 
       System.out.println("Testing Hamming distance methods");
       System.out.println("Distance between D01 and D02 is " + D01.hammingDistanceTo(D02));
@@ -1100,8 +1101,50 @@ public class Data implements Comparable<Data>
       int h = 1 + R.nextInt(6);
       System.out.println("randomSelectInNeighbour(" + h + "," + h + ") ==> " + D02.randomSelectInNeighbour(h,h));
 
-      System.out.println("------------------Mes tests Deb------------------");
-      Data testDiff1 = diff(D01, D02);
+      System.out.println("Tested by Issa SANOGO: hammingDistanceTo, randomSelectInNeighbour, diff, Control");
+      int nbOfTest =100;
+      boolean testOk = false;
+      int j = 0;
+      while (j < nbOfTest){
+         int n = 1+ R.nextInt(10);
+         boolean bit = R.nextBoolean();
+         int lu = 1 + R.nextInt(6);
+
+         if(n == 10 && bit){
+            Data A = new Data(n, bit);
+            Data B = new Data(n, bit);
+
+            Data A1 = A.randomSelectInNeighbour(lu);
+            Data B1 = B.randomSelectInNeighbour(lu);
+
+            int d = A1.hammingDistanceTo(B1);
+            Data diff = Data.diff(A1, B1);
+            int e = diff.numberOfOnes();
+
+            if (d == e) testOk = true;
+         }
+
+         if (n == 10 && !bit){
+            Data C = new Data(n, bit);
+            Data D = new Data(n, bit);
+
+            Data C1 = C.randomSelectInNeighbour(lu, lu);
+            Data D1 = D.randomSelectInNeighbour(lu, lu);
+
+            int d = C1.hammingDistanceTo(D1);
+            Data diff = Data.diff(C1, D1);
+            int e = diff.numberOfOnes();
+
+            if (d == e) testOk = true;
+
+         }
+         j++;
+         System.out.println(testOk);
+
+      }
+
+
+      /*Data testDiff1 = Data.diff(D01, D02);
       Data resExpect1 = new Data(10, true);
       if(resExpect1.equals(testDiff1))
          System.out.println("TestDiff1 ok");
@@ -1109,7 +1152,7 @@ public class Data implements Comparable<Data>
          System.out.println("TestDiff1 no ok");
 
       Data D23 = new Data("0000000000");
-      Data testDiff2 = diff(D23, D23);
+      Data testDiff2 = Data.diff(D23, D23);
       Data resExpect2 = new Data("0000000000");
       if(resExpect2.equals(testDiff2))
          System.out.println("TestDiff2 ok");
@@ -1118,7 +1161,7 @@ public class Data implements Comparable<Data>
 
       Data D24 = new Data("0101010101");
       Data D25 = new Data("1010101010");
-      Data testDiff3 = diff(D24, D25);
+      Data testDiff3 = Data.diff(D24, D25);
       Data resExpect3 = new Data("1111111111");
       if(resExpect3.equals(testDiff3))
          System.out.println("TestDiff3 ok");
@@ -1140,8 +1183,8 @@ public class Data implements Comparable<Data>
       //Control(D, true, 0) tous les bits qui sont à 1(=true) dans D deviennent 1(=p=0)
       Data D28 = new Data("0000011111");
       Data D29 = new Data("1111111111");
-      Data testControl1 = control(D28, false, 1.0);
-      Data testControl2 = control(D28, false, 0.0);
+      Data testControl1 = Data.control(D28, false, 1.0);
+      Data testControl2 = Data.control(D28, false, 0.0);
       if(D28.equals(testControl1))
          System.out.println("testControl1 ok");
       else
@@ -1154,8 +1197,8 @@ public class Data implements Comparable<Data>
 
       Data D30 = new Data("1010101010");
       Data D31 = new Data("0000000000");
-      Data testControl3 = control(D30, true, 1.0);
-      Data testControl4 = control(D30, true, 0.0);
+      Data testControl3 = Data.control(D30, true, 1.0);
+      Data testControl4 = Data.control(D30, true, 0.0);
       if(D30.equals(testControl3))
          System.out.println("testControl3 ok");
       else
@@ -1164,7 +1207,7 @@ public class Data implements Comparable<Data>
       if(D31.equals(testControl4))
          System.out.println("testControl4 ok");
       else
-         System.out.println("testControl4 no ok");
+         System.out.println("testControl4 no ok");*/
 
       System.out.println("------------------Mes tests Fin------------------");
 
